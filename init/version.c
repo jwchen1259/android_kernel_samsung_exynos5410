a4ab8e6bfede9f1f96ef82c85258990d98a7b6bd
jwchen1259 <jwchen1259@gmail.com>
2016-09-29 10:07:40 +0700

    i9500: v7.0 defconfig changes

--- init/version.c
+++ init/version.c
@@ -39,10 +39,10 @@ EXPORT_SYMBOL_GPL(init_uts_ns);
 
 /* FIXED STRINGS! Don't touch! */
 const char linux_banner[] =
-	"Linux version " UTS_RELEASE " (jwchen. (╯°□°）╯︵ )"
+	"Linux version " UTS_RELEASE " (jwchen<3)"
 	"(" LINUX_COMPILER ") " UTS_VERSION "\n";
 
 const char linux_proc_banner[] =
 	"%s version %s"
-	" (jwchen. (╯°□°）╯︵ )"
+	" (jwchen<3)"
 	" (" LINUX_COMPILER ") %s\n";
